a7eba5e5b417d6a3267a78a2d8ac9cc76be0b93f
Balamurali M <balamurali@erpnext.com>
2020-10-26 15:40:35 +0530

    feat(SalesInvoice): Add quantity field and use Transaction controller

--- accounting/accounting/doctype/sales_invoice/sales_invoice.py
+++ accounting/accounting/doctype/sales_invoice/sales_invoice.py
@@ -3,8 +3,19 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
-from frappe.model.document import Document
 
-class SalesInvoice(Document):
-	pass
+import frappe
+
+from accounting.controllers.transaction import Transaction
+
+
+class SalesInvoice(Transaction):
+
+	def __init__(self):
+		self.debit_account = frappe.get_doc("Account", "Sales")
+		self.credit_account = self.account
+
+	def on_submit(self):
+		self.amount = self.item.price * self.quantity
+		super().on_submit()
+		# update item qty
