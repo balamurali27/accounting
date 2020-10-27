ecdcb98e72230b99ead6f78a7551e47693484e00
Balamurali M <balamurali@erpnext.com>
2020-10-27 14:32:53 +0530

    test(SalesInvoice): Add test for sales invoice

--- accounting/accounting/doctype/sales_invoice/test_sales_invoice.py
+++ accounting/accounting/doctype/sales_invoice/test_sales_invoice.py
@@ -3,8 +3,30 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
-import unittest
+import frappe
 
-class TestSalesInvoice(unittest.TestCase):
-	pass
+from ..customer.test_customer import create_test_customer
+from ..item.test_item import create_test_item
+from accounting.controllers.test_transaction import TestTransaction
+
+
+def get_test_sales_invoice(
+	item=create_test_item(), customer=create_test_customer()
+):
+	return frappe.get_doc({
+		"doctype": "Sales Invoice",
+		"item": item.name,
+		"quantity": 1,
+		"customer": customer.name
+	})
+
+
+class TestSalesInvoice(TestTransaction):
+
+	def test_invoice_submission_creates_balanced_gl_entries(self):
+		"""Ensure submitting Sales Invoice creates balanced GL entries."""
+		sales_invoice = get_test_sales_invoice()
+		sales_invoice.submit()
+		gl_count = frappe.db.count("GL Entry")
+		self.assertEqual(gl_count, 2, msg=frappe.get_all("GL Entry"))
+		self._test_gl_entries_are_balanced()
