3d282563a39d38d5b8f9a0743f6101541c08f1d3
Balamurali M <balamurali@erpnext.com>
2020-10-28 21:25:04 +0530

    feat(JournalEntry): Add functionality to JournalEntry
    - check entries on balanced before submit
    - make GL entries after submit

--- accounting/accounting/doctype/journal_entry/journal_entry.py
+++ accounting/accounting/doctype/journal_entry/journal_entry.py
@@ -3,8 +3,42 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+from frappe import _
+from datetime import date
+
+import frappe
+
 from frappe.model.document import Document
 
+
 class JournalEntry(Document):
-	pass
+
+	def _check_for_balanced_entries(self):
+		debit_sum = 0
+		credit_sum = 0
+		for account in self.accounts:
+			debit_sum += account.debit
+			credit_sum += account.credit
+		if debit_sum != credit_sum:
+			frappe.throw(
+				msg=_(
+					"Total credits don't equal debits. Make them equal and try again."
+				),
+				exc=frappe.exceptions.LinkValidationError,
+			)
+
+	def before_submit(self):
+		self._check_for_balanced_entries()
+
+	def _make_gl_entries_for_accounts(self):
+		for account in self.accounts:
+			frappe.get_doc(
+				doctype="GL Entry",
+				posting_date=date.today(),
+				account=account.account,
+				debit=account.debit,
+				credit=account.credit
+			).insert()
+
+	def on_submit(self):
+		self._make_gl_entries_for_accounts()
