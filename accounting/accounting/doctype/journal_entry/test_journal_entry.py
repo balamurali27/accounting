190e26ab6b8ed77b57a3744a68485c15e36eb798
Balamurali M <balamurali@erpnext.com>
2020-10-28 20:45:48 +0530

    test(JournalEntry): Test GL entries are created

--- accounting/accounting/doctype/journal_entry/test_journal_entry.py
+++ accounting/accounting/doctype/journal_entry/test_journal_entry.py
@@ -3,8 +3,43 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
-import unittest
+from datetime import date
+from typing import List
 
-class TestJournalEntry(unittest.TestCase):
-	pass
+import frappe
+
+from accounting.controllers.test_transaction import TestTransaction
+
+from ..account.test_account import create_test_accounts
+
+
+def get_test_journal_entry_account(credit: int = 0, debit: int = 0):
+	create_test_accounts()
+	return frappe.get_doc({
+		"doctype": "Journal Entry Account",
+		"account": "Debtors",
+		"debit": debit,
+		"credit": credit
+	})
+
+
+def get_test_journal_entry(accounts: List):
+	return frappe.get_doc({
+		"doctype": "Journal Entry",
+		"accounts": accounts,
+		"posting_date": date.today()
+	})
+
+
+class TestJournalEntry(TestTransaction):
+
+	def test_invoice_submission_creates_balanced_gl_entries(self):
+		"""Ensure submitting Sales Invoice creates balanced GL entries."""
+		accounts = [get_test_journal_entry_account().as_dict()]
+		journal_entry = get_test_journal_entry(accounts)
+		before = frappe.db.count("GL Entry")
+		journal_entry.submit()
+		after = frappe.db.count("GL Entry")
+		gl_count_increase = after - before
+		self.assertEqual(gl_count_increase, 2, msg=frappe.get_all("GL Entry"))
+		self._test_gl_entries_are_balanced()
