0d3fd9b4f9433e354781a8e361832eab1f7cfaf2
Balamurali M <balamurali@erpnext.com>
2020-10-29 16:26:39 +0530

    feat(Transaction): Add voucher info to GL Entries
    - Move GL Entry creation to its own method

--- accounting/controllers/transaction.py
+++ accounting/controllers/transaction.py
@@ -13,22 +13,22 @@ from frappe.model.document import Document
 class Transaction(Document):
 	"""Generic controller for double entry transactions."""
 
-	def _make_gl_entries(self, amount: int):
+	def _make_gl_entry(self, account: str, debit: float = 0, credit: float = 0):
+		"""Make GL Entry for today with voucher info."""
+		frappe.get_doc(
+			doctype="GL Entry",
+			posting_date=date.today(),
+			account=account,
+			debit=debit,
+			credit=credit,
+			voucher_type=self.doctype,
+			voucher_no=self.name
+		).insert()
+
+	def _make_gl_entries(self, amount: float):
 		try:
-			frappe.get_doc(
-				doctype="GL Entry",
-				posting_date=date.today(),
-				account=self.debit_account,
-				debit=amount,
-				credit=0
-			).insert()
-			frappe.get_doc(
-				doctype="GL Entry",
-				posting_date=date.today(),
-				account=self.credit_account,
-				credit=amount,
-				debit=0
-			).insert()
+			self._make_gl_entry(self.debit_account, debit=amount)
+			self._make_gl_entry(self.credit_account, credit=amount)
 		except frappe.exceptions.ValidationError:
 			frappe.db.rollback()
 
