364cbaf3f17df3de4e7f4a1f813a2209f7c0603d
Balamurali M <balamurali@erpnext.com>
2020-11-04 12:17:31 +0530

    feat: Add button to show General Ledger entries of transaction

--- accounting/accounting/doctype/sales_invoice/sales_invoice.js
+++ accounting/accounting/doctype/sales_invoice/sales_invoice.js
@@ -2,7 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Sales Invoice', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function (frm) {
+		console.log(frm.doc.name)
+		frm.add_custom_button(__('Accounting Ledger'), function () {
+			frappe.set_route('query-report', 'General Ledger', {
+				voucher_type: 'Sales Invoice',
+				voucher_no: frm.doc.name,
+			});
+		});
+	},
 });
