c8336704ed50e7cc7495e225251fdeba47dd0e1d
Balamurali M <balamurali@erpnext.com>
2020-11-08 19:41:16 +0530

    refactor(WebShop): Use promises correctly add more DRY

--- accounting/www/catalog.js
+++ accounting/www/catalog.js
@@ -1,6 +1,13 @@
 buyBtn = document.getElementById('buy-btn');
 buyBtn.addEventListener('click', orderCart);
 
+var api_req_headers = {
+	'Content-Type': 'application/json',
+};
+
+if (window.csrf_token)
+	api_req_headers['X-Frappe-CSRF-Token'] = window.csrf_token;
+
 function addItemToCart(cart, item_qty) {
 	item = item_qty.id;
 	qty = item_qty.valueAsNumber;
@@ -11,48 +18,88 @@ function createSalesInvoice(cart) {
 	const options = {
 		method: 'POST',
 		body: JSON.stringify(cart),
-		headers: {
-			'Content-Type': 'application/json',
-		},
+		headers: api_req_headers,
 	};
-	if (window.csrf_token) {
-		options.headers['X-Frappe-CSRF-Token'] = window.csrf_token;
-		console.log('csrf_token is ' + window.csrf_token);
-		console.log(JSON.stringify(cart));
-	}
-	const url = document.location.origin + '/api/resource/Sales%20Invoice';
+	let url = document.location.origin + '/api/resource/Sales%20Invoice';
 	fetch(url, options)
-		.then((res) => res.json())
-		.then(showInvoicePDF);
+		.then(validateResponse)
+		.then(showInvoicePDF)
+		.catch((err) => console.log(err));
 }
 
-function showError(res) {
-	return frappe.msgprint({
-		title: __('Error'),
+function validateResponse(res) {
+	if (res.ok) return res.json();
+	showHTTPErr(res);
+	return Promise.reject(res);
+}
+
+function showHTTPErr(res) {
+	frappe.msgprint({
+		title: 'HTTP Error: ' + res.status + ' ' + res.statusText,
 		indicator: 'red',
 		message: __('Something went wrong. Try again.'),
 	});
 }
 
 function showInvoicePDF(res) {
-	doc = res.data
+	doc = res.data;
 	let url =
 		document.location.origin +
 		'/api/method/frappe.utils.print_format.download_pdf?doctype=Sales%20Invoice&';
 	url += 'name=' + doc.name + '&';
 	url += 'format=Sales%20Invoice&no_letterhead=0&_lang=en';
 	win = window.open(url, '_blank');
-  win.focus();
+	win.focus();
 }
 
-function orderCart() {
+async function getCustomer(user) {
+	let url = document.location.origin + '/api/resource/Customer';
+	url += '?filters=';
+	url += '[["user", "=", "' + user + '" ]]';
+
+	return fetch(url)
+		.then(validateResponse)
+		.then((res) => (res.data.length > 0 ? res.data[0].name : null))
+		.catch((err) => console.log(err));
+}
+
+async function createCustomer(user) {
+	customer = { user: user };
+	const options = {
+		method: 'POST',
+		body: JSON.stringify(customer),
+		headers: api_req_headers,
+	};
+	let url = document.location.origin + '/api/resource/Customer';
+	return fetch(url, options)
+		.then(validateResponse)
+		.catch((err) => console.log(err));
+}
+
+async function getUser() {
+	let url =
+		document.location.origin + '/api/method/frappe.auth.get_logged_user';
+	return fetch(url)
+		.then(validateResponse)
+		.then((res) => res.message)
+		.catch((err) => console.log(err));
+}
+
+async function setCustomer(user) {
+	customer = getCustomer(user);
+	if (!customer) customer = createCustomer(user);
+	return customer;
+}
+
+async function orderCart() {
+	user = await getUser();
+	customer = await setCustomer(user);
 	cart_item_qtys = document.querySelectorAll('.cart-item-qty');
 	cart = {};
 	cart.items = [];
-	for (i = 0; i < cart_item_qtys.length; i++) {
+	for (i = 0; i < cart_item_qtys.length; i++)
 		addItemToCart(cart, cart_item_qtys[i]);
-	}
-	cart.customer = 'balu';
+	cart.customer = customer;
 	//to submit document
 	cart.docstatus = true;
 	createSalesInvoice(cart);
