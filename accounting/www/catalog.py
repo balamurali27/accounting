5617f0ddb43154a45fc47db80bf0550bfcf81985
Balamurali M <balamurali@erpnext.com>
2020-11-09 07:30:58 +0530

    feat(WebShop): Make shop available to logged in users only

--- accounting/www/catalog.py
+++ accounting/www/catalog.py
@@ -1,8 +1,12 @@
 from __future__ import unicode_literals
+
 import frappe
+from frappe import _
 
 
 def get_context(context):
+	if frappe.session.user == "Guest":
+		frappe.throw(_("Log in to access this page."), frappe.PermissionError)
 	context.items = frappe.get_all(
 		"Item", fields=['name', 'item_name', 'description', 'price', 'image']
 	)
